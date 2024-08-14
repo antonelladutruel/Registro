4bead6870102568ec5248e88e2e67d48171a6039
cabrerarovetta <162040722+cabrerarovetta@users.noreply.github.com>
2024-08-14 19:09:12 -0300

    Update validacion.js
    if _  validación

--- js/validacion.js
+++ js/validacion.js
@@ -5,3 +5,12 @@ function showAlertSuccess() {
 function showAlertError() {
     document.getElementById("alert-danger").classList.add("show");
 }
+
+if (password1 === password2) {
+    // Mostrar alerta de éxito si las contraseñas coinciden
+    alertSuccess.classList.add('show');
+} else {
+    // Mostrar alerta de error si las contraseñas no coinciden
+    alertDanger.classList.add('show');
+}
+}
